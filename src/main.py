7295e7476589fb3aee5836c4463ad94d7f75e2f4
ErickBCruz <erickbaezcruz@gmail.com>
2025-05-31 18:39:21 -0500

    add template engine

--- src/main.py
+++ src/main.py
@@ -8,6 +8,10 @@ from src.utils.http_error_handler import HTTPErrorHandler
 from src.routers.movie_router import movie_router
 from src.routers.game_router import game_router
 
+#motores de plantillas
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import os
 
 app = FastAPI()
 
@@ -20,12 +24,21 @@ app.add_middleware(HTTPErrorHandler)
 #     print ('middleware is running!')
 #     return await call_next(request)
 
+#plantilla
+static_path = os.path.join(os.path.dirname(__file__),"static/")
+templates_path = os.path.join(os.path.dirname(__file__),"templates")
+
+app.mount("/static", StaticFiles(directory=static_path), "static")
+templates = Jinja2Templates(directory=templates_path)
+
+
 app.title = "Prueba FastApiBoost"
 
 #home
 @app.get("/", tags=["Home"], status_code=500, response_description="Esto debe devolver un error") # codigo de estado esperado, por defecto en 200, 500 es un error
-def home():
-    return PlainTextResponse(content="home", status_code=200) # Codigo de estado
+def home(request: Request):
+    #return PlainTextResponse(content="home", status_code=200) # Codigo de estado
+    return templates.TemplateResponse("index.html", {"request":request,"message":"Welcome"})
 
 #incluir routers
 app.include_router(prefix="/movies", router=movie_router)
